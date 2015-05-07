7ae4671fd40a415a4d3f586d3eccfb3104ed5223
Monir <goblortikus-github@yahoo.com>
2015-05-06 18:40:29 -0700

    Initial Commit

---
+++ dashboardmonirmamoun/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  dashboardmonirmamoun
+//
+//  Created by MM on 5/6/15.
+//  Copyright (c) 2015 MM. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
